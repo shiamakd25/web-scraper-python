9621eb280df4030f1e534e5d04520187c3704a11
shiamakd25 <shiamakdas@outlook.com>
2024-09-09 19:43:41 -0400

    Scrape and store data

--- main.py
+++ main.py
@@ -1,11 +1,41 @@
 import requests
 import selectorlib
+import time
 
 URL = "http://programmer100.pythonanywhere.com/tours/"
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
 def scrape(url):
     """Scrape the page source from the url"""
-    response = requests.get(url)
+    response = requests.get(url, headers=HEADERS)
     source = response.text
     return source
 
+def extract(source):
+    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+    value = extractor.extract(source)["tours"]
+    return value
+
+def notify():
+    print("Event found!")
+
+def store(extracted):
+    with open("data.txt", "a") as file:
+        file.write(extracted +"\n")
+
+def read(extracted):
+    with open("data.txt", "r") as file:
+        return file.read()
+
+if __name__ == "__main__":
+    while True:
+        scraped = scrape(URL)
+        extracted = extract(scraped)
+        print(extracted)
+        content = read(extracted)
+        if extracted != "No upcoming tours":
+            if not extracted in content:
+                store(extracted)
+                notify()
+        time.sleep(30)
